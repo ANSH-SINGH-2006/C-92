d340028709ac077f14beced35f60e057a234358f
Ansh Singh <64472289+ANSH-SINGH-2006@users.noreply.github.com>
2021-04-27 16:52:51 +0530

    Add files via upload

---
+++ screens/EncryptScreen.js
@@ -0,0 +1,58 @@
+import React,{Component} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+  FlatList
+
+} from 'react-native'
+
+export default class EncryptScreen extends React.Component{
+  render(){
+    return(
+        <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>Learn Here!</Text>
+        </View>
+    )
+}
+}
+
+const styles = StyleSheet.create({
+    keyBoardStyle : {
+      flex:1,
+      alignItems:'center',
+      justifyContent:'center'
+    },
+    formTextInput:{
+      width:250,
+      height:35,
+      alignSelf:'center',
+      borderColor:'#ffab91',
+      borderRadius:10,
+      borderWidth:2,
+      marginTop:20,
+      padding:10,
+    },
+    button:{
+      width:"75%",
+      height:50,
+      justifyContent:'center',
+      alignItems:'center',
+      borderRadius:10,
+      backgroundColor:"#ff5722",
+      shadowColor: "#000",
+      shadowOffset: {
+         width: 0,
+         height: 8,
+      },
+      shadowOpacity: 0.44,
+      shadowRadius: 10.32,
+      elevation: 16,
+      marginTop:20
+      },
+    }
+  )
